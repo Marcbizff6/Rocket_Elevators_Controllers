e56cfdce5a469cf32e0292bedb1e215b8a67670b
Marcbizff6 <Marcbizff6@users.noreply.github.com>
2020-02-17 11:00:01 -0500

    Adding some test files

---
+++ marc.py
@@ -0,0 +1,15 @@
+import mymodule1
+import mymodule2 as qwer
+import platform
+
+mymodule1.greeting("Jonathan")
+qwer.name("Marc")
+
+a = qwer.person1["age"]
+print(a)
+
+
+x = platform.system()
+print(x)
+
+print(platform.python_version())
